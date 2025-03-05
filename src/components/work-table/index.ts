871f9cf7c1f096edc12fd549df48856834af44ef
StoneZol <miha2000rus@gmail.com>
2025-03-05 18:49:09 +0300

    push deploy

--- src/components/work-table/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as WorkTable } from "./work-table";
--- src/components/work-table/table-modules/control-buttons/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ControlButtons } from "./control-buttons";
-export type { ControlButtonsProps } from "./control-buttons";
